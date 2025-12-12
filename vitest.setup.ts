96c3865471f7c818d214cf4fa48607189f4f5165
Jan Šimeček <simecekjann@gmail.com>
2025-12-12 10:03:43 +0100

    Mock env in tests for them to pass

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,5 @@
 import * as matchers from '@testing-library/jest-dom/vitest';
-import { beforeAll, afterEach, afterAll, expect } from 'vitest';
+import { beforeAll, afterEach, afterAll, expect, vi } from 'vitest';
 
 import { server } from './api/__test__/__mocks__/node';
 import { MOCK_CNB_BASE_URL } from './api/__test__/test-constants';
@@ -13,3 +13,9 @@ afterAll(() => server.close());
 beforeAll(() => {
   process.env.CNB_RATES_BASE_URL = MOCK_CNB_BASE_URL;
 });
+
+vi.mock('@t3-oss/env-core', () => ({
+  createEnv: vi.fn().mockReturnValue({
+    VITE_APP_URL: 'https://example.com',
+  }),
+}));
